5b9bf934b28505d26f38086b9f78e8d5cff62268
spacehhy <hehychild@gmail.com>
2020-02-10 12:07:44 +0800

    edit supplier,binaryOperator demo

--- src/main/java/com/hhy/jdk8/binaryoperator/package-info.java
+++ src/main/java/com/hhy/jdk8/binaryoperator/package-info.java
@@ -1,8 +1,9 @@
 package com.hhy.jdk8.binaryoperator;
 
 /**
- *
- *
+ * BinaryOperator (二元运算符)
+ * 表示一种操作针对相同类型的两个操作数,并且返回一个与操作数相同类型的结果
+ *  BinaryOperator<T> extends BiFunction<T,T,T>
  *
  *
  *
--- src/main/java/com/hhy/jdk8/supplier/SupplierTest.java
+++ src/main/java/com/hhy/jdk8/supplier/SupplierTest.java
@@ -22,5 +22,7 @@ public class SupplierTest {
 
         Supplier<Student> supplier2 = Student::new;
         System.out.println(supplier2.get().getAge());
+
+        /*如果构造方法引用不提供相应的构造(如空参构造),编译报错*/
     }
 }
